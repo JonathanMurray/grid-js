6ec4e4300d4183b9870b0adb4bf0b4d4961b6043
Jonathan <jonte.murray@gmail.com>
2023-11-26 18:48:09 +0100

    Add more input commands to 'less'

--- programs/less.js
+++ programs/less.js
@@ -35,7 +35,8 @@ async function run(fileName) {
     let termsize;
     let lineWidth;
     let rows;
-    let lineBeginnings;
+    let lineToRowMapping;
+    let inputBuffer = "";
 
     await write(ansiCursorPosition(999, 999));
     await write(ANSI_GET_CURSOR_POSITION);
@@ -46,18 +47,21 @@ async function run(fileName) {
 
     async function init() {
         lineWidth = termsize[0] - leftMargin;
+        let lineBeginnings;
         ({rows, lineBeginnings} = doc.calculateWrapped(lineWidth));
+
+        let lineNumber = 1;
+        lineToRowMapping = {};
+        for (let rowIndx of lineBeginnings) {
+            lineToRowMapping[lineNumber] = rowIndx;
+            lineNumber ++;
+        }
     }
 
     async function onresize() {
-        console.log("ON RESIZE");
-
         termsize = await syscall("getTerminalSize");
-        console.log("TERM SIZE: ", termsize);
         await init();
-        console.log("INITED");
         await render();
-        console.log("RENDERED");
     }
 
     handleTerminalResizeSignal(onresize);
@@ -66,15 +70,13 @@ async function run(fileName) {
 
     async function render() {
         let output = ANSI_ERASE_ENTIRE_SCREEN;
-        let lineNumber = 0;
+        let lineNumber = 1;
         for (let i = 0; i < rows.length; i++) {
 
-            if (lineBeginnings.includes(i)) {
-                lineNumber ++;
-            }
+            const lineBeginning = lineToRowMapping[lineNumber] == i;
 
             if (i >= offset) {
-                if (lineBeginnings.includes(i)) {
+                if (lineBeginning) {
                     output +=  `${ANSI_CSI}36;44m${lineNumber.toString().padStart(lineNumberWidth)}${ANSI_CSI}39;49m `;
                 } else {
                     output += `${ANSI_CSI}44m${"".padStart(lineNumberWidth)}${ANSI_CSI}49m `;
@@ -82,9 +84,26 @@ async function run(fileName) {
                 output += rows[i].padEnd(lineWidth)
             }
 
+            if (i - offset == termsize[1] - 2) {
+                break;
+            }
+
+            if (lineBeginning) {
+                lineNumber ++;
+            }
         }
-        output += ansiCursorPosition(termsize[1], termsize[0]);
+
+        // Make sure input buffer is shown at the very bottom, even for short files.
+        for (let i = 0; i < termsize[1] - rows.length; i++) {
+            output += "\n";
+        }
+
+        output += `:${inputBuffer}`;
         await write(output);
+    }
+
+    function setOffset(value) {
+        offset = Math.max(0, Math.min(value, rows.length - termsize[1]));
     }
 
     await init();
@@ -93,14 +112,40 @@ async function run(fileName) {
         await render();
         const input = await read();
 
-        const scrollSpeed = 1;
-        if (input == ANSI_CURSOR_UP) {
-            offset = Math.max(offset - scrollSpeed, 0);
-        } else if (input == ANSI_CURSOR_DOWN) {
-            offset = Math.max(0, Math.min(offset + scrollSpeed, rows.length - termsize[1]));
+        let isNumber = /^\d+$/.test(input);
+        if (isNumber) {
+            inputBuffer += input;
         } else {
-            console.log("TODO: handle input: ", input);
+
+            const lineNumber = Number.parseInt(inputBuffer);
+            
+            if ([ANSI_CURSOR_UP, "k"].includes(input)) {
+                const delta = lineNumber >= 1 ? lineNumber : 1;
+                setOffset(offset - delta);
+            } else if ([ANSI_CURSOR_DOWN, "j"].includes(input)) {
+                const delta = lineNumber >= 1 ? lineNumber : 1;
+                setOffset(offset + delta);
+            } else if (input == "g") {
+                if (lineNumber >= 1) {
+                    setOffset(lineToRowMapping[lineNumber]);
+                } else {
+                    setOffset(0);
+                }
+            } else if (input == "G") {
+                if (lineNumber >= 1) {
+                    setOffset(lineToRowMapping[lineNumber]);
+                } else {
+                    setOffset(rows.length - 1);
+                }
+            } else if (input == "q") {
+                // Exit program
+                break;
+            } else {
+                console.log("TODO: handle input: ", input);
+            }
+            inputBuffer = "";
         }
+   
     }
 }
 
