a4e3c34e20c760ef0e447cde9683f709a957124e
Jonathan <jonte.murray@gmail.com>
2023-12-15 17:49:10 +0100

    Improve ls output

--- programs/ls.js
+++ programs/ls.js
@@ -40,11 +40,21 @@ async function main(args) {
             await writeln(`${type.padEnd(widestType)} ${len.padStart(widestLength)} ${fileName}`);
         }
     } else {
+        const terminalSize = await syscall("getTerminalSize");
+        const terminalWidth = terminalSize[0];
+        const interval = 15;
+        let lineWidth = 0;
         for (let fileName of fileNames) {
-            await write(`${fileName}  `)   ;
+            const len = Math.ceil(fileName.length / interval) * interval;
+            const aligned = fileName.padEnd(len, " ");
+            if (lineWidth + aligned.length > terminalWidth) {
+                await writeln("");
+                lineWidth = 0;
+            }
+            await write(aligned);
+            lineWidth += aligned.length;
         }
         await writeln("");
     }
 }
-
 
