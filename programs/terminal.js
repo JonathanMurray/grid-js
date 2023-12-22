559fffb29fe503ecd91ba3f433307a14ec3b12af
Jonathan <jonte.murray@gmail.com>
2023-12-22 01:47:48 +0100

    Turn terminal into gui app
    improve reactivity of alternate-screen apps, like top

--- programs/terminal.js
+++ programs/terminal.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import { reportCrash } from "/lib/errors.mjs";
+import { Container, Expand, getEvents, init, redraw } from "/lib/gui.mjs";
 import { writeln, write, createWindow } from "/lib/stdlib.mjs";
 import { syscall } from "/lib/sys.mjs";
 import { TerminalGrid } from "/lib/terminal-grid.mjs";
@@ -44,32 +45,21 @@ async function main(args) {
     // We need to be leader in order to create a PTY
     await syscall("joinNewSessionAndProcessGroup");
 
-    const ctx = canvas.getContext("2d");
-    
-    let cellSize = [10, 20];
-
-    let terminalSize = [Math.floor(canvas.width / cellSize[0]), Math.floor(canvas.height / cellSize[1])];
-
-    let terminalGrid = new TerminalGrid(terminalSize, "black", "white");
-
+    const terminalGrid = new TerminalGrid([canvas.width, canvas.height], "black", "white", [10, 20]);
+    const rootContainer = new Container({bg: "black", padding: 2, expand: Expand.YES});
+    rootContainer.addChild(terminalGrid);
 
     const ptyMaster = await syscall("openFile", {path: "/dev/ptmx"});
     const slaveNumber = await syscall("controlDevice", {fd: ptyMaster, request: {getSlaveNumber: null}});
     const ptySlave = await syscall("openFile", {path: `/dev/pts/${slaveNumber}`});
 
-    await syscall("controlDevice", {fd: ptyMaster, request: {resize: {width: terminalSize[0], height: terminalSize[1]}}});
+    await syscall("controlDevice", {fd: ptyMaster, request: {resize: {width: terminalGrid.numColumns, height: terminalGrid.numRows}}});
 
     let childPid;
 
     await syscall("handleInterruptSignal");
 
-    function draw() {
-        ctx.fillStyle = terminalGrid.defaultBackground;;
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        terminalGrid.draw(ctx, cellSize);
-    }
-
-    draw();
+    await init(rootContainer, graphicsFd, canvas);
 
     try {
         childPid = await syscall("spawn", {programPath: childProgramPath, fds: [ptySlave, ptySlave],
@@ -80,135 +70,12 @@ async function main(args) {
         const childPgid = childPid; // The child is process group leader
         await syscall("controlDevice", {fd: ptyMaster, request: {setForegroundPgid: childPgid}});
 
-        async function recomputeTerminalSize() {
-            terminalSize = [Math.floor(canvas.width/ cellSize[0]), Math.floor(canvas.height / cellSize[1])];
-            terminalGrid.resize(terminalSize);
-            draw();
-            await syscall("controlDevice", {fd: ptyMaster, request: {resize: {width: terminalSize[0], height: terminalSize[1]}}});
-        }
-
-        async function changeFontSize(modifier) {
-            // TODO Careful with float canvas coordinates. Can they cause bad antialiasing effects?
-            cellSize = [cellSize[0] * modifier, cellSize[1] * modifier];
-            await recomputeTerminalSize();
-        }
-
-        async function onwindowWasResized(event) {
-            canvas.width = event.width;
-            canvas.height = event.height;
-
-            await recomputeTerminalSize();
-
-            const msg = JSON.stringify({resizeDone: null});
-            await write(msg, graphicsFd);
-        };
-
-        async function oncloseWasClicked(event) {
-            await writeln("Terminal shutting down. (Window was closed.)");
-            await syscall("exit");
-        };
-
-        function onwheel(event) {
-            const updated = terminalGrid.scroll(event.deltaY);
-            if (updated) {
-                draw();
-            }
-        };
-
-        function onmenubarButtonWasClicked({buttonId}) {
-            if (buttonId == "ZOOM_IN") {
-                changeFontSize(1.11);
-            } else {
-                assert(buttonId == "ZOOM_OUT");
-                changeFontSize(0.9);
-            }
-        };
-        
-        function onmenubarDropdownItemWasClicked({itemId}) {
-            if (itemId == "DARK") {
-                terminalGrid.setDefaultBackground("black");
-                terminalGrid.setDefaultForeground("white");
-            } else if (itemId == "LIGHT") {
-                terminalGrid.setDefaultBackground("white");
-                terminalGrid.setDefaultForeground("black");
-            } else if (itemId == "MATRIX") {
-                terminalGrid.setDefaultBackground("black");
-                terminalGrid.setDefaultForeground("#00FF00");
-            } else {
-                assert(false);
-            }
-            draw();
-        };
-
-
-        async function onkeydown(event) {
-
-            const key = event.key;
-            let sequence;
-    
-            if (event.ctrlKey && key == "c") {
-                sequence = ASCII_END_OF_TEXT;
-            } else if(event.ctrlKey && key == "d") {
-                sequence = ASCII_END_OF_TRANSMISSION;
-            } else if (event.ctrlKey && key == "-") {
-                changeFontSize(0.9);
-            } else if (event.ctrlKey && key == "+") {
-                changeFontSize(1.11);
-            } else if (key == "Backspace") {
-                sequence = ASCII_BACKSPACE;
-            } else if (key == "ArrowUp") {
-                sequence = ANSI_CURSOR_UP;
-            } else if (key == "ArrowDown") {
-                sequence = ANSI_CURSOR_DOWN;
-            } else if (key == "ArrowRight") {
-                sequence = ANSI_CURSOR_FORWARD;
-            } else if (key == "ArrowLeft") {
-                sequence = ANSI_CURSOR_BACK;
-            } else if (key == "Home") {
-                sequence = ASCII_CARRIAGE_RETURN;
-            } else if (key == "End") {
-                sequence = ANSI_CURSOR_END_OF_LINE;
-            } else if (key == "Enter") {
-                sequence = "\n";     
-            } else if (key == "Shift") {
-                sequence = null;
-            } else if (key.length > 1) {
-                //console.log("Unhandled key in terminal: ", key);
-                sequence = null;
-            } else {
-                sequence = key;
-            }
-    
-            if (sequence != null) {
-                await write(sequence, ptyMaster);
-            }
-        };
-
-        async function handleGraphicsMessages(messages) {
-            const parsed = JSON.parse(messages);
-            for (const {name, event} of parsed) {
-                if (name == "keydown") {
-                    await onkeydown(event);
-                } else if (name == "menubarDropdownItemWasClicked") {
-                    onmenubarDropdownItemWasClicked(event);
-                } else if (name =="windowWasResized") {
-                    await onwindowWasResized(event);
-                } else if (name =="closeWasClicked") {
-                    await oncloseWasClicked(event);
-                } else if (name =="wheel") {
-                    onwheel(event);
-                } else if (name =="menubarButtonWasClicked") {
-                    onmenubarButtonWasClicked(event);
-                }
-            }
-        }
-    
         while (true) {
             const readyFd = await syscall("pollRead", {fds: [graphicsFd, ptyMaster]});
             if (readyFd == graphicsFd) {
-                const messages = await syscall("read", {fd: graphicsFd});
-                await handleGraphicsMessages(messages);
-                draw();
+                for await (const {name, event} of getEvents()) {
+                    await handleGuiEvent({name, event});
+                }
                 continue;
             }
 
@@ -268,11 +135,11 @@ async function main(args) {
                 }
             }
             
-            draw();
+            await redraw();
         }
 
         terminalGrid.insert("\nThe child process has shut down. This terminal can't be used anymore.");
-        draw();
+        await redraw();
 
         while (true) {
             await syscall("sleep", {millis: 60000});
@@ -298,5 +165,113 @@ async function main(args) {
     } finally {
         console.log("Terminal shutting down");
     }
+    
+    async function changeFontSize(modifier) {
+        // TODO Careful with float canvas coordinates. Can they cause bad antialiasing effects?
+        terminalGrid.setCellSize([terminalGrid.cellSize[0] * modifier, terminalGrid.cellSize[1] * modifier]);
+        await redraw();
+        await syscall("controlDevice", {fd: ptyMaster, request: {resize: {width: terminalGrid.numColumns, height: terminalGrid.numRows}}});
+    }
+
+    async function onwindowWasResized(event) {
+        canvas.width = event.width;
+        canvas.height = event.height;
+
+        await redraw();
+        await syscall("controlDevice", {fd: ptyMaster, request: {resize: {width: terminalGrid.numColumns, height: terminalGrid.numRows}}});
+        await write(JSON.stringify({resizeDone: null}), graphicsFd);
+    };
+
+    async function oncloseWasClicked(event) {
+        await writeln("Terminal shutting down. (Window was closed.)");
+        await syscall("exit");
+    };
+
+    function onmenubarButtonWasClicked({buttonId}) {
+        if (buttonId == "ZOOM_IN") {
+            changeFontSize(1.11);
+        } else {
+            assert(buttonId == "ZOOM_OUT");
+            changeFontSize(0.9);
+        }
+    };
+    
+    async function onmenubarDropdownItemWasClicked({itemId}) {
+        if (itemId == "DARK") {
+            terminalGrid.setDefaultBackground("black");
+            terminalGrid.setDefaultForeground("white");
+        } else if (itemId == "LIGHT") {
+            terminalGrid.setDefaultBackground("white");
+            terminalGrid.setDefaultForeground("black");
+        } else if (itemId == "MATRIX") {
+            terminalGrid.setDefaultBackground("black");
+            terminalGrid.setDefaultForeground("#00FF00");
+        } else {
+            assert(false);
+        }
+        await redraw();
+    };
+
+    async function handleGuiEvent({name, event}) {
+        if (name == "keydown") {
+            await onkeydown(event);
+        } else if (name == "menubarDropdownItemWasClicked") {
+            await onmenubarDropdownItemWasClicked(event);
+        } else if (name =="windowWasResized") {
+            await onwindowWasResized(event);
+        } else if (name =="closeWasClicked") {
+            await oncloseWasClicked(event);
+        } else if (name =="menubarButtonWasClicked") {
+            onmenubarButtonWasClicked(event);
+        } else if (name == "mousedown") {
+            console.log("TODO mousedown", event);
+        }
+    }
+
+    async function onkeydown(event) {
+
+        const key = event.key;
+        let sequence;
+
+        if (event.ctrlKey && key == "c") {
+            sequence = ASCII_END_OF_TEXT;
+        } else if(event.ctrlKey && key == "d") {
+            sequence = ASCII_END_OF_TRANSMISSION;
+        } else if (event.ctrlKey && key == "-") {
+            changeFontSize(0.9);
+        } else if (event.ctrlKey && key == "+") {
+            changeFontSize(1.11);
+        } else if (key == "Backspace") {
+            sequence = ASCII_BACKSPACE;
+        } else if (key == "ArrowUp") {
+            sequence = ANSI_CURSOR_UP;
+        } else if (key == "ArrowDown") {
+            sequence = ANSI_CURSOR_DOWN;
+        } else if (key == "ArrowRight") {
+            sequence = ANSI_CURSOR_FORWARD;
+        } else if (key == "ArrowLeft") {
+            sequence = ANSI_CURSOR_BACK;
+        } else if (key == "Home") {
+            sequence = ASCII_CARRIAGE_RETURN;
+        } else if (key == "End") {
+            sequence = ANSI_CURSOR_END_OF_LINE;
+        } else if (key == "Enter") {
+            sequence = "\n";     
+        } else if (key == "Shift") {
+            sequence = null;
+        } else if (key.length > 1) {
+            //console.log("Unhandled key in terminal: ", key);
+            sequence = null;
+        } else {
+            sequence = key;
+        }
+
+        if (sequence != null) {
+            await write(sequence, ptyMaster);
+        }
+    };
+
+
+
 
 }
